db1a81be62ddb0ec914637184352260890976f30
Pitta <shuna120700@gmail.com>
2024-10-22 16:20:18 +0800

    commit

--- singbirds_project/settings.py
+++ singbirds_project/settings.py
@@ -130,6 +130,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
