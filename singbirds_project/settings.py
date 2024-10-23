5970b900fe81fec340576f1f855afa29caba3cb6
Pitta <shuna120700@gmail.com>
2024-10-23 23:40:41 +0800

    Update settings.py

--- singbirds_project/settings.py
+++ singbirds_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
