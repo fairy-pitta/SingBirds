d2af02720e82b7b8987530abf5335bbaef93eb8e
Pitta <shuna120700@gmail.com>
2024-10-23 23:35:49 +0800

    Update settings.py

--- singbirds_project/settings.py
+++ singbirds_project/settings.py
@@ -142,7 +142,6 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 1000000
 # ]
 
 try:
-    from ..local_settings import *
+    from .local_settings import *
 except ImportError:
-    # local_settings.py が存在しなくてもエラーにならないようにする
     pass
